305295997c344dcfd709bfdf0978e72bfd4ac4ea
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-06-23 10:52:14 -0700

    HappyPet

--- src/HappyPet.java
+++ src/HappyPet.java
@@ -4,26 +4,56 @@ public class HappyPet {
 	
 	// 2. Add the following variable to the next line: static int happinessLevel = 0;
 	// this will be used to store the happiness of your pet
+	static int happinessLevel = 0;
 	
 	public static void main(String[] args) {
 		// 1. Ask the user what kind of pet they want to buy, and store their answer in a variable
-
+String pet= JOptionPane.showInputDialog("What kind of pet do you want to buy?");
 		// 7. REPEAT steps 3 - 6 enough times to make your pet happy!
-		
+		for (int i = 0; i < 16263837; i++) {
+			
+			
 			// 3. Use showOptionDialog to ask the user what they want to do to make their pet happy
 			//    (eg: cuddle, food, water, take a walk, groom, clean up poop).
 			//    Make sure to customize the title and question too.
-			int task = JOptionPane.showOptionDialog(null, "Question", "Title", 0, JOptionPane.INFORMATION_MESSAGE, null,
-					new String[] { "Button1", "Button2", "Button3" }, null);
-
+			int task = JOptionPane.showOptionDialog(null, "What do you want to do to make your pet happy?", "Title", 0, JOptionPane.INFORMATION_MESSAGE, null,
+					new String[] { "poop", "Cuddle ", "Take a Walk" }, null);
+				
 			// 5. Use user input to call the appropriate method created in step 4.
-
+			System.out.println(task);
+			if (task==0) { poop();
+			}
+			
+			if (task==1) { cuddle();
+			
+			}
+			if (task==2) { walk();
+			
+			}
 			// 6. If you determine the happiness level is large enough, tell the
 			//    user that he loves his pet and use break; to exit for loop.
-
+			if(happinessLevel>10) {
+				JOptionPane.showMessageDialog(null, "You love your pet.");
+				break; 
+			}
+		}				
+			
 	}
 
 	// 4. Create methods to handle each of your user selections.
 	//    Each method should create a pop-up with the pet's response (eg. cat might purr when pet), 
 	//    and INCREMENT the pet's happiness Level.
+	static void walk() {
+		JOptionPane.showMessageDialog(null, "It will run.");
+	happinessLevel+=5;
+	}
+	static void cuddle() {
+		JOptionPane.showMessageDialog(null, "It will fall asleep.");
+	happinessLevel+=5;
+	}
+	static void poop() {
+		JOptionPane.showMessageDialog(null, "It will be happy.");
+		happinessLevel+=3;
+		
+	}
 }
