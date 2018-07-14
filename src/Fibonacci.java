06e0ee228c9e6b37f24bd273fc75a09bfa0b61ba
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-07-14 10:53:38 -0700

    FibocciniPrimeOrNot

---
+++ src/Fibonacci.java
@@ -0,0 +1,13 @@
+
+public class Fibonacci {
+public static void main(String[] args) {
+	int a=
+	int b=
+	int c=
+	for (int i = 0; i < 13; i++) {
+		
+		 
+		
+	}
+}
+}
--- src/PrimeOrNot.java
+++ src/PrimeOrNot.java
@@ -4,8 +4,14 @@ public class PrimeOrNot {
 public static void main(String[] args) {
 String number=	JOptionPane.showInputDialog("Give me a number.");
 int prime= Integer.parseInt(number);
-for (int i = 2; i < 5; i++) {
-	
+for (int i = 2; i < prime; i++) {
+	if (prime%i==0){
+		JOptionPane.showMessageDialog(null, "Not a prime!!!!!!!");
+		System.exit(0);}
+}
+	JOptionPane.showMessageDialog(null, "Its a prime number!!!!!!!!!!");
+	System.exit(0);
 }
 }
-}
+
+
