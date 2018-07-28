852b78982e5fcb088c31d307d498b18025c49eb8
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-07-28 10:56:33 -0700

    fibonacci

--- src/Fibonacci.java
+++ src/Fibonacci.java
@@ -1,10 +1,18 @@
 
 public class Fibonacci {
 public static void main(String[] args) {
-	int a=
-	int b=
-	int c=
+	int first= 1;
+	int second= 1;
+	int third=0; 
+	System.out.println(first);
+		System.out.println(second);
 	for (int i = 0; i < 13; i++) {
+		
+		third=first+second;
+		System.out.println(third);
+		first=second;
+		second=third;
+		
 		
 		 
 		
